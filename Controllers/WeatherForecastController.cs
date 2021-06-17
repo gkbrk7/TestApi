cdb2bd3e1cc661c56000d1eae6419760681aaf7e
gkbrk7 <48270546+gkbrk7@users.noreply.github.com>
2021-06-17 15:06:35 +0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Heavy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
